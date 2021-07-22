9469b0e6d5f9a003d2a4dccad790ab6c91da12a7
UbayGD <ubay.glezdiaz@gmail.com>
2021-07-22 14:47:10 +0100

    Change stack for notebook + modify the GPU section

--- src/slimbookamdcontroller.py
+++ src/slimbookamdcontroller.py
@@ -9,8 +9,9 @@ import gettext, locale, time
 import shutil
 import configparser
 import re #Busca patrones expresiones regulares
-import pyamdgpuinfo
 from pathlib import Path
+
+from services.gpu_service import GpuService
 
 gi.require_version('Gtk', '3.0')
 gi.require_version('Gdk', '3.0')
@@ -113,7 +114,7 @@ class SlimbookAMD(Gtk.ApplicationWindow):
 
         grid = Gtk.Grid(column_homogeneous=False,
                          column_spacing=0,
-                         row_spacing=10)
+                         row_spacing=12)
         grid.set_name('label')
         self.add(grid) 
 
@@ -162,7 +163,7 @@ class SlimbookAMD(Gtk.ApplicationWindow):
     # CPU ---------------------------------------------------------------------------------
         cpuGrid = Gtk.Grid(column_homogeneous=False,
                          column_spacing=0,
-                         row_spacing=10)
+                         row_spacing=12)
         cpuGrid.set_name('cpuGrid')
 
         # Consumo Procesador
@@ -234,72 +235,75 @@ class SlimbookAMD(Gtk.ApplicationWindow):
 
 
     # GPU --------------------------------------------------------------------------------
-        # exists_gpus = pyamdgpuinfo.detect_gpus()
-        gpu = pyamdgpuinfo.get_gpu(0)
-        gpu_name = Gtk.Label(label=gpu.name)
-        gpu_memory_size = Gtk.Label(label='{} GB'.format(self._convert_bytes_to_gbytes(gpu.memory_info['vram_size'])))
-        gpu_slot = Gtk.Label(label=gpu.pci_slot)
-        gpu_temp = Gtk.Label(label='{}º C'.format(self.get_gpu_temp(gpu.gpu_id)))
+        def add_gpus_pages(notebook: Gtk.Notebook):
+            number_of_gpus = GpuService.get_number_of_gpus()
+            for gpu_Index in range(number_of_gpus):
+                page = Gtk.Box()
+                page.set_border_width(10)
+                page.add(build_gpu_listbox(gpu_Index))
+                notebook.append_page(page, Gtk.Label(label="GPU {}".format(gpu_Index)))
+            return
+        
+        def build_gpu_listbox(gpu_Index: int) -> Gtk.Box:
+            gpu_model = GpuService.get_model(gpu_Index)
+            gpu_memory_size = GpuService.get_vram(gpu_Index, 'GB')
+            gpu_slot = GpuService.get_slot(gpu_Index)
+            gpu_temp = GpuService.get_temp(gpu_Index)
 
-        box_outer = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-        self.add(box_outer)
+            box_outer = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
 
-        listbox = Gtk.ListBox()
-        listbox.set_selection_mode(Gtk.SelectionMode.NONE)
-        box_outer.pack_start(listbox, True, True, 0)
+            listbox = Gtk.ListBox()
+            listbox.set_selection_mode(Gtk.SelectionMode.NONE)
+            box_outer.pack_start(listbox, True, True, 0)
 
-        row = Gtk.ListBoxRow()
-        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
-        row.add(hbox)
-        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-        hbox.pack_start(vbox, True, True, 0)
+            row = Gtk.ListBoxRow()
+            hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
+            row.add(hbox)
+            vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+            hbox.pack_start(vbox, True, True, 0)
 
-        label_gpu_model = Gtk.Label(label="Model", xalign=0)
-        vbox.pack_start(label_gpu_model, True, True, 0)
-        hbox.pack_start(gpu_name, False, True, 0)
-        listbox.add(row)
+            label_gpu_model = Gtk.Label(label="Model", xalign=0)
+            vbox.pack_start(label_gpu_model, True, True, 0)
+            hbox.pack_start(Gtk.Label(label=gpu_model), False, True, 0)
+            listbox.add(row)
 
-        row = Gtk.ListBoxRow()
-        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
-        row.add(hbox)
-        label_memory_size = Gtk.Label(label="VRAM", xalign=0)
-        hbox.pack_start(label_memory_size, True, True, 0)
-        hbox.pack_start(gpu_memory_size, False, True, 0)
-        listbox.add(row)
+            row = Gtk.ListBoxRow()
+            hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
+            row.add(hbox)
+            label_memory_size = Gtk.Label(label="VRAM", xalign=0)
+            hbox.pack_start(label_memory_size, True, True, 0)
+            hbox.pack_start(Gtk.Label(label=gpu_memory_size), False, True, 0)
+            listbox.add(row)
 
-        row = Gtk.ListBoxRow()
-        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
-        row.add(hbox)
-        label_temp = Gtk.Label(label="Temp", xalign=0)
-        hbox.pack_start(label_temp, True, True, 0)
-        hbox.pack_start(gpu_temp, False, True, 0)
-        listbox.add(row)
+            row = Gtk.ListBoxRow()
+            hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
+            row.add(hbox)
+            label_temp = Gtk.Label(label="Temp", xalign=0)
+            hbox.pack_start(label_temp, True, True, 0)
+            hbox.pack_start(Gtk.Label(label=gpu_temp), False, True, 0)
+            listbox.add(row)
 
-        row = Gtk.ListBoxRow()
-        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
-        row.add(hbox)
-        label_pci_slot = Gtk.Label(label="PCI Slot", xalign=0)
-        hbox.pack_start(label_pci_slot, True, True, 0)
-        hbox.pack_start(gpu_slot, False, True, 0)
-        listbox.add(row)
+            row = Gtk.ListBoxRow()
+            hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
+            row.add(hbox)
+            label_pci_slot = Gtk.Label(label="PCI Slot", xalign=0)
+            hbox.pack_start(label_pci_slot, True, True, 0)
+            hbox.pack_start(Gtk.Label(label=gpu_slot), False, True, 0)
+            listbox.add(row)
+            return box_outer
 
-    # STACK ----------------------------------------------------------------------------------
-        vboxStack = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
-        vboxStack.set_halign(Gtk.Align.CENTER)
-        self.add(vboxStack)
+    # NOTEBOOK ----------------------------------------------------------------------------------
+        notebook = Gtk.Notebook()
+        self.add(notebook)
 
-        stack = Gtk.Stack()
-        stack.set_transition_type(Gtk.StackTransitionType.SLIDE_LEFT_RIGHT)
-        stack.set_transition_duration(1000)
+        page1 = Gtk.Box()
+        page1.set_orientation(Gtk.Orientation.HORIZONTAL)
+        page1.set_border_width(10)
+        page1.add(cpuGrid)
+        notebook.append_page(page1, Gtk.Label(label="CPU"))
 
-        stack.add_titled(cpuGrid, "cpu", "CPU")
-
-        stack.add_titled(box_outer, "gpu", "GPU")
-
-        stack_switcher = Gtk.StackSwitcher()
-        stack_switcher.set_stack(stack)
-        vboxStack.pack_start(stack_switcher, True, True, 0)
-        vboxStack.pack_start(stack, True, True, 0)
+        if GpuService.exists_amd_gpus():
+            add_gpus_pages(notebook)
 
 
     # BUTTONS --------------------------------------------------------------------------------
@@ -370,7 +374,7 @@ class SlimbookAMD(Gtk.ApplicationWindow):
         grid.attach(switch2, 7, 6, 2, 1)
         grid.attach(separador2, 2, 7, 8, 1)
 
-        grid.attach(vboxStack, 2, 10, 10, 1)
+        grid.attach(notebook, 0, 10, 12, 1)
         
         grid.attach(botonesBox, 6, 20, 1, 1)
         grid.attach(evnt_box, 9, 20, 2, 1)
@@ -639,12 +643,6 @@ class SlimbookAMD(Gtk.ApplicationWindow):
             config.write(configfile)
 
         print("\n- Variable |"+variable+"| updated in .conf, actual value: "+value)
-
-    def _convert_bytes_to_gbytes(self, vram_size):
-        return round(float(vram_size)/1024.0**3)
-
-    def get_gpu_temp(self, gpuIndex):
-        return pyamdgpuinfo.get_gpu(gpuIndex).query_temperature()
 
 win = SlimbookAMD()
 
