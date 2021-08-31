1b514772e109012cd6bd0a4af1671b3544ece3ae
slimbook <dev@slimbook.es>
2021-08-31 16:45:20 +0200

    Removig extra files

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(slimbookamdcontrolleralpha):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="slimbookamdcontrolleralpha" command="/usr/bin/slimbookamdcontroller"
--- debian/slimbookamdcontrolleralpha.cron.d.ex
+++
@@ -1,5 +0,0 @@
-#
-# Regular cron jobs for the slimbookamdcontroller package
-#
-0 4	* * *	root	[ -x /usr/bin/slimbookamdcontroller_maintenance ] && /usr/bin/slimbookamdcontroller_maintenance
-s
