1feb5e4301c4ce2c755b1609b2f95e203101d427
UbayGD <ubay.glezdiaz@gmail.com>
2022-11-18 19:14:28 +0000

    add regexs for extract the model name from lspci output

--- src/constants/gpu_constants.py
+++ src/constants/gpu_constants.py
@@ -9,6 +9,10 @@ GPU_VOLT = 'GPU Voltage'
 
 DYNAMIC_GPU_PROPERTIES = [TEMP, GPU_FREQ, MEM_FREQ, VRAM_USAGE, GPU_VOLT]
 
-UNSUPPORTED_GPU_MODELS = [
-    'Radeon Vega Series / Radeon Vega Mobile Series'
-]
+UNKNOWN_MODEL = 'Unknown model'
+
+REGEX_MODEL_WITH_MODEL_IN_BRACKETS_MATCH = r"Advanced\sMicro\sDevices,\sInc\..\[AMD\/ATI\]\s[a-zA-Z0-9\/\s]+\s\[[a-zA-Z0-9\s\/]+\]"
+REGEX_MODEL_WITH_MODEL_IN_BRACKETS_FINDALL = r"\s\[[a-zA-Z0-9\s\/]+\]"
+
+REGEX_MODEL_ONLY_SERIES_NAME_MATCH = r"Advanced\sMicro\sDevices,\sInc\..\[AMD\/ATI\]\s[a-zA-Z0-9\s\/]+\s\([a-zA-Z0-9\s\/]+\)"
+REGEX_MODEL_ONLY_SERIES_NAME_FINDALL = r"\s[a-zA-Z0-9\s\/]+\s"
