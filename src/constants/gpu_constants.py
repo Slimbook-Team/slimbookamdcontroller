bea20f4de267855315cae7c0724fe6a690dd961f
Slimbook <dev@slimbook.es>
2023-04-05 11:01:14 +0200

    Development beta (#28)
    * layout refactor and improved graph
    
    * Add mokutil as a dependency
    
    * Fix slimbookamdcontroller leaves zombie processes #19
    
    * show shorter name for unsupported gpu models
    
    * round gpu voltage to 3 decimal places
    
    * add regexs for extract the model name from lspci output
    
    ---------
    
    Co-authored-by: slimbook <marlerin@slimbook.es>
    Co-authored-by: UbayGD <ubay.glezdiaz@gmail.com>
    Co-authored-by: Mar <89640378+MarSlimbook@users.noreply.github.com>

--- src/constants/gpu_constants.py
+++ src/constants/gpu_constants.py
@@ -8,3 +8,11 @@ PCI_SLOT = 'PCI Slot'
 GPU_VOLT = 'GPU Voltage'
 
 DYNAMIC_GPU_PROPERTIES = [TEMP, GPU_FREQ, MEM_FREQ, VRAM_USAGE, GPU_VOLT]
+
+UNKNOWN_MODEL = 'Unknown model'
+
+REGEX_MODEL_WITH_MODEL_IN_BRACKETS_MATCH = r"Advanced\sMicro\sDevices,\sInc\..\[AMD\/ATI\]\s[a-zA-Z0-9\/\s]+\s\[[a-zA-Z0-9\s\/]+\]"
+REGEX_MODEL_WITH_MODEL_IN_BRACKETS_FINDALL = r"\s\[[a-zA-Z0-9\s\/]+\]"
+
+REGEX_MODEL_ONLY_SERIES_NAME_MATCH = r"Advanced\sMicro\sDevices,\sInc\..\[AMD\/ATI\]\s[a-zA-Z0-9\s\/]+\s\([a-zA-Z0-9\s\/]+\)"
+REGEX_MODEL_ONLY_SERIES_NAME_FINDALL = r"\s[a-zA-Z0-9\s\/]+\s"
